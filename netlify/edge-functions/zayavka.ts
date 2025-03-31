21af2393d6ec2dfc651ca6be963d4bc9607176c0
A.Rudin <a.rudin@berg.ru>
2025-04-01 00:16:37 +0300

    another tg bot

--- netlify/edge-functions/zayavka.ts
+++ netlify/edge-functions/zayavka.ts
@@ -5,7 +5,7 @@ export default async (req: Request, { cookies, geo }: Context) => {
     const url = new URL(req.url);
 
     const res = await fetch(
-        `https://api.telegram.org/bot2080569073:AAFbriREPchioL2nLd9szAGWbse9uMF8BEU/sendMessage?chat_id=4548832048&text=Имя: ${url.searchParams.get("name")} \n Телефон: ${url.searchParams.get("tel")}`,
+        `https://api.telegram.org/bot2057943408:AAGCwN2zwC9d01ORsFsyDwCFF2Hk1zYGSUk/sendMessage?chat_id=297138711&text=Имя: ${url.searchParams.get("name")} \n Телефон: ${url.searchParams.get("tel")}`,
         {
           method: "GET",
           headers: {
