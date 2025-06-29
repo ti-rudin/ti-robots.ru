f4b221ace1c6b2a5daac1bbd1a2cbb745cd63f50
A.Rudin <a.rudin@berg.ru>
2025-06-29 17:37:36 +0300

    `Update Telegram group link in Contacts.vue`

--- src/views/Contacts.vue
+++ src/views/Contacts.vue
@@ -76,7 +76,7 @@
           <p class="text-gray-600 dark:text-gray-300 mb-4">
             {{ translations.spamNotice[currentLanguage] }}
           </p>
-          <a href="https://t.me/yourtelegramgroup" target="_blank"
+          <a href="https://t.me/ti_robots_lab" target="_blank"
             class="inline-flex items-center px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition-colors">
             <svg class="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 24 24">
               <path d="M12 0C5.373 0 0 5.373 0 12s5.373 12 12 12 12-5.373 12-12S18.627 0 12 0zm5.894 8.221-1.97 9.28c-.145.658-.537.818-1.084.508l-3-2.21-1.446 1.394c-.14.14-.26.26-.534.26l.192-3.19 5.542-5.04c.24-.213-.054-.334-.373-.12l-6.87 4.33-2.96-.92c-.64-.203-.658-.64.135-.954l11.57-4.46c.538-.196 1.006.128.832.94z"/>
